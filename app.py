ef926c12fae258e7bcd982ac9a22e89fc8caeafd
IvanZadorozhniy <i.zadorozniy@yahoo.com>
2022-02-14 19:00:31 +0200

    Added some error handler

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from database.db import initialize_db
 from resources.routes import initialize_routes
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from resources.error import errors
 
 
 app = Flask(__name__)
@@ -15,7 +16,7 @@ app.config['SQLALCHEMY_BINDS'] = []
 app.config['CSRF_ENABLED'] = True
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config['UPLOAD_FOLDER'] = 'database/upload_posters'
-api = Api(app)
+api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
--- resources/movie.py
+++ resources/movie.py
@@ -6,6 +6,9 @@ from datetime import datetime
 from database.db import db_session
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from sqlalchemy import exc
+from resources.error import SchemaValidationError, MovieAlreadyExistsError, \
+InternalServerError, UpdatingMovieError, DeletingMovieError, MovieNotExistsError
 
 class MoviesApi(Resource):
     def get(self):
@@ -16,47 +19,70 @@ class MoviesApi(Resource):
 
     @jwt_required()
     def post(self):
-        print("1")
-        user_id = get_jwt_identity()
-        print("2")
-        user = User.query.get(user_id)
-        body = request.get_json()
-        title = body['title']
-        date = body['date']
-        film = Film()
-        film.title = str(title)
-        film.date = datetime.strptime(date, '%d.%m.%Y')
-        film.user_id = user_id
-        user.films.append(film)
-        db_session.add(film)
-        db_session.commit()
-        db_session.flush()
-        return {'id': str(film.id)}, 200   
+        try:
+            user_id = get_jwt_identity()
+            user = User.query.get(user_id)
+            body = request.get_json()
+            title = body['title']
+            date = body['date']
+            film = Film()
+            film.title = str(title)
+            film.date = datetime.strptime(date, '%d.%m.%Y')
+            film.user_id = user_id
+            user.films.append(film)
+            db_session.add(film)
+            db_session.commit()
+            db_session.flush()
+            return {'id': str(film.id)}, 200
+        except exc.IntegrityError as e:
+            raise MovieAlreadyExistsError
+        except exc.SQLAlchemyError as e:
+            print(type(e))
+        except Exception as e:
+            raise InternalServerError
 
 class MovieApi(Resource):
     @jwt_required()
     def put(self, id):
-        print(id)
-        body = request.get_json()
-        film = Film.query.get(id)
-        film.title = body['title'] if 'title' in body else film.title
-        film.date =  datetime.strptime(body['date'],'%d.%m.%Y') if 'date' in body else film.date
-        db_session.commit()
-        db_session.flush()
-        return "", 200
+        try:
+            body = request.get_json()
+            film = Film.query.get(id)
+            film.title = body['title'] if 'title' in body else film.title
+            film.date =  datetime.strptime(body['date'],'%d.%m.%Y') if 'date' in body else film.date
+            db_session.commit()
+            db_session.flush()
+            return "", 200
+        except exc.InvalidRequestError as e:
+             raise SchemaValidationError
+        except exc.NoSuchTableError as e:
+            raise UpdatingMovieError
+        except exc.NoResultFound as e:
+            raise UpdatingMovieError
+        except Exception as e:
+            raise InternalServerError 
+
     @jwt_required()
     def delete(self, id):
-
-        body = request.get_json()
-        film = Film.query.get(id)
-        db_session.delete(film)
-        db_session.commit()
-        db_session.flush()
-        return "", 200
+        try:
+            body = request.get_json()
+            film = Film.query.get(id)
+            db_session.delete(film)
+            db_session.commit()
+            db_session.flush()
+            return "", 200
+        except exc.NoSuchTableError as e:
+            raise DeletingMovieError
+        except Exception as e:
+            raise InternalServerError 
 
 
     def get(self, id):
-        film = Film.query.get(id)
-        return make_response(film.to_json(), 200)
+        try:
+            film = Film.query.get(id)
+            return make_response(film.to_json(), 200)
+        except exc.NoSuchTableError as e:
+            raise MovieNotExistsError
+        except Exception as e:
+            raise InternalServerError 
 
 
